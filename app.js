47cfe7bc7706144c0c8dede7a293f43d912a57af
Sek <seksergelen470@gmail.com>
2023-07-15 02:36:26 +0800

    very latest version is ready

--- app.js
+++ app.js
@@ -61,7 +61,7 @@ document.querySelector(".btn-hold").addEventListener("click", function() {
     document.getElementById("score-" + activePlayer).textContent =
       scores[activePlayer];
   
-    if (scores[activePlayer] >= 10) {
+    if (scores[activePlayer] >= 100) {
       isNewGame= false;
   
       document.getElementById("name-" + activePlayer).textContent = "WINNER!!!";
